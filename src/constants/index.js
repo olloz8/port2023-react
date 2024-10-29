cb85d7465c2a7c5419db8b3ce76839d06cb70127
xyzli <kyu_107@naver.com>
2024-10-29 11:12:29 +0900

    1029 헤더

---
+++ src/constants/index.js
@@ -0,0 +1,23 @@
+
+const headerNav = [
+    {
+        title:"intro",
+        url:"#intro",
+    },
+    {
+        title:"skill",
+        url:"#skill",
+    },
+    {
+        title:"site",
+        url:"#site",
+    },
+    {
+        title:"portfolio",
+        url:"#port",
+    },
+    {
+        title:"contact",
+        url:"#contact",
+    },
+];
